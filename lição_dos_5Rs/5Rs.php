abba242ae1922337e8e2e6e1f345b3cbc4c4e1e5
victor <victorhs.almeida131@gmail.com>
2024-06-08 20:48:17 -0300

    .

---
+++ lição_dos_5Rs/5Rs.php
@@ -0,0 +1,117 @@
+<?php
+
+    include_once "inicio.php";
+
+    echo   '
+
+    <nav>
+        <div class="linha2">
+            <div class="pags"> <a href="index.html" class="link"> Home </a> </div>
+
+
+        </div>
+
+    </nav>
+
+
+    <main>
+        <h1 class="tito">Como preservar o meio ambiente utilizando os 5R´S:</h1>
+        <br><br><br><br><br>
+
+        <h3 class="subtito">Oque são os 5 R´s :</h3>
+        <p> Os 5 R´s da Reciclagem é uma abordagem sustentável No mundo atual, a preocupação com o
+             meio ambiente nunca foi tão crucial. À medida que nos conscientizamos dos impactos negativos
+             das nossas ações no planeta, surge uma necessidade urgente de adotar práticas mais 
+             sustentáveis em nosso estilo de vida. Nesse contexto, os 5 R´s da reciclagem emergem como um guia 
+             prático e abrangente para reduzir o desperdício, conservar recursos naturais e proteger o meio 
+             ambiente para as gerações futuras.</p> <br>
+
+        <h3 class="subtito">Quais são os 5 R´s :</h3>
+
+        <p id="lista">Os 5 R´s  consistem em 5 palavras, Reduzir, Reutilizar, Reciclar,  Repensar e Recusar. 
+             Para mais informações selecione um dos tópicos abaixo.</p>    
+
+        <a href="#1"><li>Reduzir</li></a>
+        <a href="#2"><li>Reutilizar</li></a>
+        <a href="#3"><li>Reciclar</li></a>
+        <a href="#4"><li>Repensar</li></a>
+        <a href="#5"><li>Recusar</li></a>
+            
+        <br><br><br><br>
+
+        <h4 id="1">1.Reduzir:</h4>
+        <p>A primeira etapa dos 5 R´s é reduzir. Isso implica em diminuir a quantidade de resíduos que geramos em primeiro lugar.
+             Ao optar por produtos com menos embalagens, comprar apenas o necessário e
+             evitar o desperdício, estamos reduzindo nossa pegada ambiental desde o início.</p>
+             
+        <li><a href="#lista">voltar ao inicio</a></li><br>
+            
+        <h4 id="2">2.Reutilizar:</h4>
+        <p> Reutilizar é a segunda etapa e envolve dar uma segunda vida aos produtos e materiais sempre que possível.
+             Isso pode incluir doar roupas, móveis e eletrônicos usados, utilizar recipientes reutilizáveis
+             em vez de descartáveis e até mesmo encontrar novos usos criativos para itens antigos.</p>
+            
+        <li><a href="#lista">voltar ao inicio</a></li><br>
+      
+        <h4 id="3">3.Reciclar:</h4>
+        <p>O terceiro "R" é a reciclagem, uma prática bem conhe e fundamen tal para o ciclo de vida dos produtos.
+             Reciclar envolve coletar, processar e transformar materiais usados em novos produtos. Isso reduz a necessidade de extrair
+             recursos naturais virgens e minimiza a quantidade de resíduos enviados para aterros sanitários.</p>
+     
+        <li><a href="#lista">voltar ao inicio</a></li><br>
+      
+        <h4 id="4">4.repensar:</h4>
+        <p>Repensar é uma etapa menos óbvia, mas igualmente importante. Envolve questionar nossos padrões de consumo e considerar alternativas
+             mais sustentáveis em nossas escolhas diárias. Ao repensar nossos hábitos de compra e estilo de vida, podemos reduzir 
+             significativamente nosso impacto ambiental.</p>
+                
+        <li><a href="#lista">voltar ao inicio</a></li><br>
+        
+        <h4 id="5">5.recusar:</h4>
+        <p>O último "R" é recusar. Isso significa recusar produtos desnecessários que geram desperdício ou têm um impacto ambiental 
+             negativo significativo. Ao recusar produtos descartáveis, excessivamente embalados ou com pouca durabilidade, estamos 
+             enviando uma mensagem clara de que valorizamos a sustentabilidade e a preservação do meio ambiente.</p>
+            
+                 <li><a href="#lista">voltar ao inicio</a></li><br>
+             
+        <h3 class="subtito">Como praticar os 5 R´s :</h3>
+        <p>Para ser mais sustentável no dia a dia, siga os 5 Rs: recusar, reduzir, reutilizar, reciclar e reparar.
+
+            Recuse coisas desnecessárias e descartáveis, como sacolas plásticas e brindes não essenciais.
+            
+            Reduza o consumo de itens descartáveis, planeje suas compras e evite o desperdício de alimentos.
+            
+            Reutilize sacolas e recipientes duráveis, e dê nova vida a itens como potes de vidro e roupas usadas.
+            
+            Recicle materiais como papel, plástico, vidro e metal, e escolha produtos feitos com materiais reciclados sempre que possível.
+            
+            Repare itens quebrados em vez de descartá-los, e aprenda habilidades básicas de conserto para prolongar sua vida útil.
+            
+            Seguindo essas práticas, você estará fazendo sua parte para reduzir o desperdício e proteger o meio ambiente.</p> <br>
+
+        
+
+
+    
+    </main>
+
+    <footer>
+       <hr>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <JK> © 2024 Victor e Felipe </JK>
+                </div>
+                <div class="direitos">
+                    <JK>(19) 12345-6789</JK>
+                </div>
+                <div class="link">
+                    <a href="construcao.html">Sobre Nós</a> <j>/</j>  
+                    <a href="">Termos De Uso</a>  
+                </div>
+        </div>
+    </footer>
+</body>
+</html>'
+
+?>
---
+++ lição_dos_5Rs/includes/inicio.php
@@ -0,0 +1,24 @@
+<?php
+    echo '<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Natura - VF</title>
+    
+    <link href="estilos/principal.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+    <header>            
+        <div class="linha">
+            <div class="caixa"> <h1>
+                <sep class="p1">Natureza V</sep>
+                <sep class="p2">F</sep></h1></div>
+            <div class="caixa1"> <a href="login.htm" class="link"> Login/cadastre-se </a> </div>
+        </div>
+        
+        <hr id="lhdr">
+    </header>';
